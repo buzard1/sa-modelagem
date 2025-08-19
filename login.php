5be980a4e27310d5e62fe2db5fa7288f7ce02ce6
matheus anjos <matheusdlanjos@hotmail.com>
2025-08-19 15:14:58 -0300

    versao 1
    matheus amaral

--- login.php
+++ login.php
@@ -1,3 +1,43 @@
+<?php
+session_start();
+require_once 'conexao.php';
+
+if ($_SERVER['REQUEST_METHOD']== "POST") {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT * FROM usuario WHERE email = :email";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($usuario && password_verify($senha, $usuario['senha'])){
+        // LOGIN BEM SUCEDIDO DEFINE AS VARIÁVEIS DE SESSÃO
+        $_SESSION['usuario'] = $usuario['nome'];
+        $_SESSION['perfil'] = $usuario['id_perfil'];
+        $_SESSION['id_usuario'] = $usuario['id_usuario'];
+
+
+        // VERIFICA SE A SENHA É TEMPORÁRIA
+        if ($usuario['senha_temporaria']){
+        // REDIRECIONA PARA A TROCA DE SENHA
+        header("location: alterar_senha.php");
+        exit();
+} else {
+        // REDIRECIONA PARA A PÁGINA PRINCIPAL
+        header("location: principal.php");
+        exit();
+}
+    }else{
+        // LOGIN INVALIDO
+        echo "<script>alert('E-mail ou senha inválidos!');window.location.href = 'login.php';
+        </script>";
+    } 
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
